cd2537ee98dc1aa53ab326c120f557d98153138b
Paloma Toledo <palomatoledo997@gmail.com>
2023-11-10 22:36:36 -0300

    agrego mas imagenes, creo seccion de clases y realizo correcciones de la enterega anterior

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// let nombre = prompt ("Ingrese su nombre, por favor");
+
+// let saludo =  "hola " + paloma;
+
+// alert (saludo);
+
+// let edadUsuario = prompt ("ingrese su edad");
+
+
+// if (edadUsuario==25){
+//     alert("usted tiene 25")
+// } else if ( edadUsuario < 25) {
+//     alert ("usted es joven, no lo desperdicie")
+// } else (edadUsuario >25) 
+// { alert ("estamos grandes")};
+
+;
+
+// for (let i= 1; i<10 ; i++) {
+//     let nombre = prompt ("ingresar su nombre");
+//     alert ("Turno N°" + i + "Nombre: " + nombre); 
+// }
