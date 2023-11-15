6402eb90ad810e819b3dca0d9c63fd16fdebb28c
Paloma Toledo <palomatoledo997@gmail.com>
2023-11-14 21:41:29 -0300

    agrego animaciones con javaScript, agrego las cards con filtros de js a la page de Classes

--- js/script.js
+++ js/script.js
@@ -1,22 +1,46 @@
-// let nombre = prompt ("Ingrese su nombre, por favor");
+document.addEventListener("keyup", (e) => { 
+    if (e.target.matches(".filtro-carta")) { //declaro condicion para cada vez que se presione la tecla
+        document.querySelectorAll(".card_classes").forEach((tarjeta) =>{
+            tarjeta.textContent.toLocaleLowerCase().includes(e.target.value) //paso todo el contenido de cards a minuscula
+            ?tarjeta.classList.remove("filter") //si da true, remueve la clase filtro
+            :tarjeta.classList.add ("filter"); //
+        })
+    } //cuando el usuario se posicione sobre el "search" busque entre las cards
+})
 
-// let saludo =  "hola " + paloma;
 
-// alert (saludo);
+let animado = document.querySelectorAll(".animado");
 
-// let edadUsuario = prompt ("ingrese su edad");
+function animacionScroll () {
+    let scrollTop= document.documentElement.scrollTop;
+    for (let i=0; i<animado.length; i++) {
+        let altura = animado [i].offsetTop;
+        if (altura -500 < scrollTop) {
+            animado [i].style.opacity = 1;
+        }
+    }
+}
 
+window.addEventListener ("scroll", animacionScroll);
 
-// if (edadUsuario==25){
-//     alert("usted tiene 25")
-// } else if ( edadUsuario < 25) {
-//     alert ("usted es joven, no lo desperdicie")
-// } else (edadUsuario >25) 
-// { alert ("estamos grandes")};
+//funcion para determinar el grupo al que corresponde, segun la edad
 
-;
+function nivelPorEdad(){
+        
+    let edad = document.getElementById("edad").value;
 
-// for (let i= 1; i<10 ; i++) {
-//     let nombre = prompt ("ingresar su nombre");
-//     alert ("Turno N°" + i + "Nombre: " + nombre); 
-// }
+    if((edad >= 8)&&(edad<=12)) {
+        grupo.textContent = "Pertenece al grupo de INTANTILES";
+
+    }else if((edad >= 17)&&(edad<=70)){
+        grupo.textContent = "Pertenece al grupo ADULTOS";
+
+    }else if((edad < 8)&&(edad >70)){
+        grupo.textContent = "No contamos con grupos de esa edad";
+
+    }else{
+        grupo.textContent = "Puede tomar las clases de STRETCHING";
+
+    }
+
+}
