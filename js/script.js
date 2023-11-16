6cc62bea720139645bca44286d4d8c2730c9a39b
Paloma Toledo <palomatoledo997@gmail.com>
2023-11-16 19:00:58 -0300

    corrigo el JS de los filtros de la seccion de clases, doy ultimo retoque de css a las cards de beneficios y cambio de lugar en boton leer mas a la seccion del about us

--- js/script.js
+++ js/script.js
@@ -11,20 +11,19 @@ document.addEventListener("keyup", (e) => {
 })
 
 //añado animaciones para que al hacer scroll, vayan apareciendo los elementos del html
-let animado = document.querySelectorAll(".animado");
+let animado = document.querySelectorAll(".animado"); 
 
 function animacionScroll () {
     let scrollTop= document.documentElement.scrollTop;
     for (let i=0; i<animado.length; i++) {
         let altura = animado [i].offsetTop;
-        if (altura -550 < scrollTop) {
+        if (altura -550 < scrollTop) {  //defino a que altura aparecera el elemento 
             animado [i].style.opacity = 1;
         }
     }
 }
 
 window.addEventListener ("scroll", animacionScroll);
-
 
 //funcion para determinar el grupo al que corresponde, segun la edad
 
@@ -44,20 +43,17 @@ function nivelPorEdad(){
         
     let edad = document.getElementById("edad").value;
 
-    if((edad >= 8)&&(edad<=12)) {
+    if(edad >= 8 && edad<=12) {
         grupo.textContent = "Pertenece al grupo de INTANTILES";
 
-    }else if((edad >= 18)&&(edad<=70)){
+    }else if(edad >= 13 && edad<=17){
+        grupo.textContent = "Puede tomar las clases de STRETCHING";
+    }else if(edad >=18 && edad <=70){
         grupo.textContent = "Pertenece al grupo ADULTOS";
 
-    }else if((edad <=13)||(edad >=17)){
-        grupo.textContent = "Puede tomar las clases de STRETCHING";
-
     }else{
-        grupo.textContent = "No contamos con clases disponibles";
-
+        grupo.textContent = "Ingrese una edad entre 8 y 70";
     }
-
 }
 
 //boton ocultar para mostrar texto en las cards
